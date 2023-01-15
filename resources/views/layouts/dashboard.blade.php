98aef15c1179119c040ec5d7872702955ec07202
thiago-campos <thiagovsc96@gmail.com>
2023-01-15 14:29:21 -0400

    correção icon barra navegador dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,11 +8,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Painel Administrativo - {{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/image/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/image/favicon-16x16.png')}}">
     <meta name="base-url" content="{!!url('/')!!}"/>
     <input type="hidden" id="web-router" value="{{url('/dashboard')}}">
 
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/image/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/image/favicon-16x16.png')}}">
 
     {{-- @yield('styles') --}}
     <!-- Custom fonts for this template-->
